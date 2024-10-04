e694211d676eac574433f27877bfdf52b62cfbdd
Per <131469670+ForemostApe@users.noreply.github.com>
2024-10-04 09:26:39 +0200

    Added baseline SignalR-functionality.

---
+++ ChatAppAssignment.Api/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppAssignment.Api.Hubs
+{
+    public class ChatHub : Hub //Inheritance from Hub-class, enabling access to SignalR-functionality.
+    {
+        //Function that enables the client to invoke ReceiveMessage-method to send a message to the chat.
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
--- ChatAppAssignment.Api/Program.cs
+++ ChatAppAssignment.Api/Program.cs
@@ -1,3 +1,5 @@
+using ChatAppAssignment.Api.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Adds SignalR to DI-container.
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -21,5 +26,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//Defines URI of chat.
+app.MapHub<ChatHub>("/chat");
 
 app.Run();
