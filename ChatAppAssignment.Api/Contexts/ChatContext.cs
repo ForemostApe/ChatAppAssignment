421fb0e3182cf91ccb54cde2fa61dc0a7334fcf7
Per <131469670+ForemostApe@users.noreply.github.com>
2024-10-04 14:24:06 +0200

    Added DbContext, moved conn-string to secrets and added entities.

---
+++ ChatAppAssignment.Api/Contexts/ChatContext.cs
@@ -0,0 +1,11 @@
+﻿using ChatAppAssignment.Api.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatAppAssignment.Api.Contexts
+{
+    public class ChatContext(DbContextOptions<ChatContext> options) : IdentityDbContext<UserEntity>(options)
+    {
+        public virtual DbSet<ChatMessageEntity> Messages { get; set; }
+    }
+}
---
+++ ChatAppAssignment.Api/Entities/UserEntity.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChatAppAssignment.Api.Entities;
+
+public class UserEntity : IdentityUser
+{
+    public ICollection<ChatMessageEntity>? ChatMessages { get; set; }
+}
