f9fdbe30c4c09e18f0eef79deaa94ed270a90b57
Per <131469670+ForemostApe@users.noreply.github.com>
2024-10-06 17:30:24 +0200

    Addition of chat-view. Straight to main, which is smart. Let's test in prod as well!

---
+++ ChatAppAssignment.Clientapp/src/chat/Chat.jsx
@@ -0,0 +1,13 @@
+import "./Chat.css";
+
+const Chat = () => {
+  return (
+    <div className="container">
+      <div className="chat-grid">
+        <div className="chat-message-incoming">Hallå</div>
+        <div className="chat-message-outgoing">Hallå själv</div>
+      </div>
+    </div>
+  );
+};
+export default Chat;
