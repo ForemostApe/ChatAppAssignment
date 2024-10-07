de54705f5c1e351eb089147cc0b70922bc69c1d3
Per <131469670+ForemostApe@users.noreply.github.com>
2024-10-07 12:18:21 +0200

    Corrected the signalR-connection.

--- ChatAppAssignment.Clientapp/src/chat/Chat.jsx
+++ ChatAppAssignment.Clientapp/src/chat/Chat.jsx
@@ -1,17 +1,18 @@
 import "./Chat.css";
 import * as signalR from "@microsoft/signalr";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Chat = () => {
   const [message, setMessage] = useState("");
   const [username, setUsername] = useState("");
   const [error, setError] = useState(null);
-  const [connection, setConnection] = useState(null);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+  const connectionRef = useRef(null);
 
   useEffect(() => {
+    //Authentication from Jwt-token
     const fetchChatData = async () => {
       const token = localStorage.getItem("jwtToken");
 
@@ -36,9 +37,29 @@ const Chat = () => {
         }
 
         const data = await response.json();
-        setUsername(data.UserName);
+        setUsername(data.Username);
+
+        //Open websocket-connection using signalR.
+        if (!connectionRef.current) {
+          const conn = new signalR.HubConnectionBuilder()
+            .withUrl("https://localhost:7122/chat", {
+              accessTokenFactory: () => localStorage.getItem("jwtToken "),
+            })
+            .build();
+
+          connectionRef.current = conn;
+
+          await conn
+            .start()
+            .then(() => {
+              console.log("Connected to the hub.");
+            })
+            .catch(function (error) {
+              setError(`Failed to start the connection: ${error.message}`);
+            });
+        }
       } catch (error) {
-        setError(`Failed to connect to the chat: ${error.message}`);
+        setError(`Failed to start the connection: ${error.message}`);
         navigate("/");
       } finally {
         setLoading(false);
@@ -47,34 +68,20 @@ const Chat = () => {
 
     fetchChatData();
 
-    const conn = new signalR.HubConnectionBuilder()
-      .withUrl("https://localhost:7122/chat", {
-        accessTokenFactory: () => localStorage.getItem("jwtToken"),
-      })
-      .build();
-
-    conn
-      .start()
-      .then(() => {
-        console.log("Connected to the hub.");
-      })
-      .catch(function (error) {
-        return console.error(error.toString());
-      });
-
-    setConnection(conn);
-
     return () => {
-      conn.stop();
+      if (connectionRef.current) {
+        connectionRef.current.stop();
+        connectionRef.current = null;
+      }
     };
   }, [navigate]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="container">Loading...</div>;
   }
 
   if (error) {
-    return <div>{error}</div>;
+    return <div className="container">{error}</div>;
   }
 
   const handleSubmit = async (e) => {
@@ -85,7 +92,7 @@ const Chat = () => {
 
   async function sendMessage() {
     try {
-      await connection.invoke("SendMessage", username, message);
+      await connectionRef.current.invoke("SendMessage", username, message);
     } catch (error) {
       console.error(error.toString());
     }
