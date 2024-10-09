981c5a504f7c0f8ea89c1cd38fac0eb33f6a12c1
Per <131469670+ForemostApe@users.noreply.github.com>
2024-10-09 12:39:59 +0200

    Refactored services- and appconfigurations into extension-class.

---
+++ ChatAppAssignment.Api/Extensions/AppConfigurationExtensions.cs
@@ -0,0 +1,91 @@
+﻿using ChatAppAssignment.Api.Contexts;
+using ChatAppAssignment.Api.Entities;
+using ChatAppAssignment.Api.Factories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace ChatAppAssignment.Api.Extensions;
+
+public static class AppConfigurationExtensions
+{
+    public static IServiceCollection AddCorsPolicy(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowSpecificOrigin", policy =>
+            {
+                policy
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithOrigins("http://localhost:5173")
+                    .AllowCredentials();
+            });
+        });
+        return services;
+    }
+
+    public static IServiceCollection ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ChatContext>(options =>
+    options.UseSqlite(configuration.GetConnectionString("DbConnection")));
+        return services;
+    }
+
+    public static IServiceCollection ConfigureIdentity(this IServiceCollection services)
+    {
+        services.AddDefaultIdentity<UserEntity>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+            options.SignIn.RequireConfirmedEmail = false;
+            options.Password.RequiredLength = 8;
+
+        }).AddEntityFrameworkStores<ChatContext>();
+        return services;
+    }
+
+    public static IServiceCollection ConfigureJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtKey = configuration["Jwt:Key"];
+        var jwtIssuer = configuration["Jwt:Issuer"];
+        services.AddSingleton<TokenFactory>(sp => new TokenFactory(jwtKey, jwtIssuer));
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+         .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtIssuer,
+                ValidAudience = "http://localhost:5173",
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+            };
+        });
+        return services;
+    }
+
+    public static IApplicationBuilder ConfigureRequestPipeline(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // Configure the HTTP request pipeline.
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseCors("AllowSpecificOrigin");
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+
+        return app;
+    }
+}
